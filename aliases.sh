ddacdf8065dfb2f770eabee0c397782ee154c20b
Hyunsu Lim <hyunsu.lim01@gmail.com>
2025-02-26 11:44:44 +0900

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,4 @@
 # Remove local branches that upstreams are gone.
 alias gdel="git branch -v | grep gone | awk '{print \$1}' | xargs git branch -D"
-# Show git diff word-by-word.
-alias gwdiff="git diff -U0 --word-diff --no-index --"
+# A well-known `diff` command, but shows diff word-by-word.
+alias wdiff="git diff -U0 --word-diff --no-index --"
